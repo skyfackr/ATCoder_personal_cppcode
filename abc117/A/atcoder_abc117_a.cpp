b9f3d70d3350f9f53488f1a1c73504dc4428a3f6
Tony Jiang <44115200+skyfackr@users.noreply.github.com>
2019-02-09 22:57:34 +0800

    20190209

---
+++ abc117/A/atcoder_abc117_a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,x;
+    double ans;
+    cin>>t>>x;
+    ans=(double)t/(double)x;
+    cout<<fixed<<setprecision(10)<<ans<<endl;
+    return 0;
+}
